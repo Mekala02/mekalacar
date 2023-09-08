eaaa629295eaafe827e97ac38945d41606e199d6
mekala02 <ernkla@gmail.com>
2023-09-08 11:46:32 +0300

    add zed launch

--- launch/drive.launch.py
+++ launch/drive.launch.py
@@ -4,8 +4,11 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import PathJoinSubstitution, TextSubstitution
+import os
 
 def generate_launch_description():
+    cwd = os.getcwd()
+
     return LaunchDescription([
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([
@@ -17,6 +20,19 @@ def generate_launch_description():
             ]),
         ),
         
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare('zed_wrapper'),
+                    'launch',
+                    'zedm.launch.py'
+                ])
+            ]),
+            launch_arguments={
+                "config_path": os.path.join(cwd, 'config', 'zed_common.yaml')
+            }.items()
+        ),
+
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([
                 PathJoinSubstitution([
